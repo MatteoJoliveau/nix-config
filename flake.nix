df019438675c9db61f1b27ed9b9d13b4489b0d9f
Matteo Joliveau <matteo@matteojoliveau.com>
2023-12-16 21:05:41 +0100

    Update nixos
    Signed-off-by: Matteo Joliveau <matteo@matteojoliveau.com>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
