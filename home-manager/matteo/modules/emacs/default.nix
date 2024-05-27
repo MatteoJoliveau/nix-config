9079860279993d5ae358bd6280e915cb93c72478
Matteo Joliveau <matteo@matteojoliveau.com>
2024-05-27 11:30:32 +0200

    Remove lockfile for emacs
    Signed-off-by: Matteo Joliveau <matteo@matteojoliveau.com>

--- home-manager/matteo/modules/emacs/default.nix
+++ home-manager/matteo/modules/emacs/default.nix
@@ -8,5 +8,4 @@
 
   xdg.configFile."emacs/init.el".source = ./init.el;
   xdg.configFile."emacs/early-init.el".source = ./early-init.el;
-  xdg.configFile."emacs/straight/versions/default.el".source = ./lockfile.el;
 }
